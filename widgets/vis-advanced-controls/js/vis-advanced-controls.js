24ddbdd243b4483d4ba37fce2a947544f4530312
Bernd Nägele <bernd.naegele@sih-solutions.de>
2020-09-11 00:05:32 +0200

    0.0.14 dialog-click-button

--- widgets/vis-advanced-controls/js/vis-advanced-controls.js
+++ widgets/vis-advanced-controls/js/vis-advanced-controls.js
@@ -127,7 +127,8 @@ vis.binds.advancedui = {
         $dlg.data('preload', (!preload || preload === 'false') && !$dlg.html().replace(/\s/g, ''));
         $('[aria-describedby="' + $dlg.attr('id') + '"]').remove();
 
-        console.log('Version vis-advanced-controls: vis.binds.advancedui.dialog');
+        console.log('vis-advanced-controls: vis.binds.advancedui.dialog');
+        console.log('vis-advanced-controls: ' + $(el).parent().find('div.dialog-click-button').toString())
 
         options.width     = options.width  || options.dialog_width;
         options.height    = options.height || options.dialog_height;
@@ -251,7 +252,7 @@ vis.binds.advancedui = {
 
 // this code can be placed directly in vis-advanced-controls.html
 vis.binds['vis-advanced-controls'] = {
-    version: '0.0.13',
+    version: '0.0.14',
     showVersion: function () {
         if (vis.binds['vis-advanced-controls'].version) {
             console.log('Version vis-advanced-controls: ' + vis.binds['vis-advanced-controls'].version);
